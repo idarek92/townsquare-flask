01a6fee691775ec07dfeb0867fd0c1d6d23fbcf1
Marcin aka Sin <marcinmu330@gmail.com>
2014-12-06 16:20:58 -0600

    created index and deleted some javascript files

--- townsquare/index/__init__.py
+++ townsquare/index/__init__.py
@@ -5,12 +5,12 @@ This package handles the server side logic behind the home page.
 
 """
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 index = Blueprint('index', __name__)
 
 
 @index.route('/')
 def home():
-    return 'Townsquare'
+    return render_template('index.html')
 
